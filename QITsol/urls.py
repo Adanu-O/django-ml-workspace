d9980f7347c9d1c616ce11e50862b3a55f76f7f4
Adanu Ogbe <adanuogbe@yahoo.com>
2026-02-01 13:36:44 +0100

    Initial Django project with QITsol, LeetCode, RentPrediction

--- QITsol/urls.py
+++ QITsol/urls.py
@@ -5,6 +5,6 @@ from .views import qitsol_home, inner_product_view
 urlpatterns = [
     path('', qitsol_home, name='qitsol_home'),
     #path('inner-product/', inner_product_view, name='inner_product'),
-    path('inner_product/', inner_product_view)
+    path('inner_product/', inner_product_view),
 ]
 
